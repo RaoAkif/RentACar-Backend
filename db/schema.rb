c9ff4aa6fc551dd2b199cce23e446a40f23f1308
raoakif <akifrao@gmail.com>
2022-08-11 22:16:14 +0500

    To make it required for db, Add null: false in Users table

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_11_153434) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
